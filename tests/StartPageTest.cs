3d2bb2e4284b17cecd1e4fd3d026f51942a0e5c3
KD <kdkahlo@gmail.com>
2025-05-26 09:14:41 +0200

    SetUp & OneTimeTearDown functions created

---
+++ tests/StartPageTest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading;
+using AltTester.AltTesterUnitySDK.Driver;
+using trashcat_automation.pages;
+using NUnit.Framework;
+
+namespace trashcat_automation.tests
+{
+
+    public class StartPageTest
+    {
+        AltDriver Driver;
+        MainMenuPage startPage;
+
+        //the Setup method tells the system this is needed first
+        [SetUp]
+
+        public void SetUp()
+        {
+            Driver = new AltDriver(port: 13000);
+            startPage = new MainMenuPage(Driver);
+
+        }
+        //clean up/tear down function for when testing is complete
+        //this will stop the driver connection to the game. 
+        [OneTimeTearDown]
+
+        public void Dispose()
+        {
+            Driver.Stop();
+            //this comes from System.Threading at top of page. It is needed for Threading.
+            Thread.Sleep(100);
+        }
+        
+    }
+}
